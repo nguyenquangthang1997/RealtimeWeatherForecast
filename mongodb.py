84107a3ecfa6ced27a58e62af36b2fb2ff9ae348
thang.nq <thang.nq@orai.io>
2021-10-02 11:00:22 +0700

    Feat: remove data(1)

--- mongodb.py
+++ mongodb.py
@@ -8,3 +8,16 @@ weather_data_all_collection = weather_data["WeatherDataAll"]
 
 def insert_weather_data(datas):
     weather_data_all_collection.insert_many(datas)
+
+
+def query(_query):
+    result = []
+    responses = weather_data_all_collection.find(_query)
+    for doc in responses:
+        result.append(doc)
+    return result
+
+
+# example
+# https://www.w3schools.com/python/python_mongodb_query.asp
+print(query({"VPact(mbar)": "5.53"}))
