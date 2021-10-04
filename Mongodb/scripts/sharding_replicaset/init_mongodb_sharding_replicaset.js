d2e4fe101642cc348f5e039ca371a03f95c9d229
thang.nq <thang.nq@orai.io>
2021-10-04 21:46:12 +0700

    Feat: add save result

--- Mongodb/scripts/sharding_replicaset/init_mongodb_sharding_replicaset.js
+++ Mongodb/scripts/sharding_replicaset/init_mongodb_sharding_replicaset.js
@@ -1,5 +1,7 @@
 var addShard0 = sh.addShard("rs0/shard00:27017,shard01:27017,shard02:27017");
 var addShard1 = sh.addShard("rs1/shard03:27017,shard04:27017,shard05:27017");
-
+// sh.enableSharding("weather-data")
+// sh.shardCollection("weather-data.PredictedWeatherDataAll", {"_id": 1})
+// sh.shardCollection("weather-data.WeatherDataAll", {"_id": 1})
 printjson(addShard0);
 printjson(addShard1);
--- mongodb.py
+++ mongodb.py
@@ -4,10 +4,15 @@ mongoClient = pymongo.MongoClient("mongodb://localhost:27017/")
 weather_data = mongoClient["weather-data"]
 
 weather_data_all_collection = weather_data["WeatherDataAll"]
+predicted_weather_data_all_collection = weather_data["PredictedWeatherDataAll"]
 
 
 def insert_weather_data(datas):
     weather_data_all_collection.insert_many(datas)
+
+
+def insert_predicted_weather_data(datas):
+    predicted_weather_data_all_collection.insert_many(datas)
 
 
 def query(_query):
@@ -16,8 +21,8 @@ def query(_query):
     for doc in responses:
         result.append(doc)
     return result
-
-
-# example
-# https://www.w3schools.com/python/python_mongodb_query.asp
-print(query({"VPact(mbar)": "5.53"}))
+#
+#
+# # example
+# # https://www.w3schools.com/python/python_mongodb_query.asp
+# print(query({"VPact(mbar)": "5.53"}))
