6762a7374378b25380da1ce2271fa170fdb93753
nguyenquangthang <thang.nq@orai.io>
2021-10-06 23:12:42 +0700

    Fix: test

--- visualization/test.py
+++ visualization/test.py
@@ -10,28 +10,30 @@ def test_history():
     #          'Tdew(degC)': '-1.36', 'rh(%)': '74.30', 'VPmax(mbar)': '7.44', 'VPact(mbar)': '5.53',
     #          'VPdef(mbar)': '1.91', 'sh(g/kg)': '3.52', 'H2OC(mmol/mol)': '5.65', 'rho(g/m**3)': '1232.89',
     #          'wv(m/s)': '1.93', 'max.wv(m/s)': '4.50', 'wd(deg)': '233.30'}]
-    csv_path = mongodb.queryAll({})
-    with open("../full.json", "w") as file:
-        json.dump(csv_path,file)
-    # xlabel = 'Date time'
-    # ylabel = ''
-    # title = ''
-    # save_path = 'images/full'
-    # if not os.path.isdir(save_path):
-    #     os.mkdir(save_path)
-    # history(csv_path, save_path, xlabel, ylabel, title)
-    #
-    # # csv_path = mongodb.queryAll({"DateTime": {"$gt": 1451606400}})
-    # with open("data/full1year.json", "r") as file:
-    #     csv_path = json.load(file)
-    #
-    # xlabel = 'Date time'
-    # ylabel = ''
-    # title = ''
-    # save_path = 'images/full1year'
-    # if not os.path.isdir(save_path):
-    #     os.mkdir(save_path)
-    # history(csv_path, save_path, xlabel, ylabel, title)
+    # csv_path = mongodb.queryAll({})
+    with open("data/full.json", "r") as file:
+        csv_path = json.load(file)
+
+
+    xlabel = 'Date time'
+    ylabel = ''
+    title = ''
+    save_path = 'images/full'
+    if not os.path.isdir(save_path):
+        os.mkdir(save_path)
+    history(csv_path, save_path, xlabel, ylabel, title)
+
+    # csv_path = mongodb.queryAll({"DateTime": {"$gt": 1451606400}})
+    with open("data/full1year.json", "r") as file:
+        csv_path = json.load(file)
+
+    xlabel = 'Date time'
+    ylabel = ''
+    title = ''
+    save_path = 'images/full1year'
+    if not os.path.isdir(save_path):
+        os.mkdir(save_path)
+    history(csv_path, save_path, xlabel, ylabel, title)
 
 
 def test_drawpredict():
@@ -56,27 +58,27 @@ def test_drawpredict():
     # })
     # print(len(truth))
 
-    pred = mongodb.queryPredict(
-        {
-            "$or": [
-                {
-                    "$and":
-                        [
-                            {"DateTime": {"$lt": 1477353610}},
-                            {"DateTime": {"$mod": [24 * 3600, 0]}}
-                        ]
-                },
-                {
-                    "$and":
-                        [
-                            {"DateTime": {"$gt": 1477353600}},
-                            {"DateTime": {"$mod": [24 * 3600, 58200]}}
-                        ]
-                }
-            ]
-        }
-    )
-    print(len(pred))
+    # pred = mongodb.queryPredict(
+    #     {
+    #         "$or": [
+    #             {
+    #                 "$and":
+    #                     [
+    #                         {"DateTime": {"$lt": 1477353610}},
+    #                         {"DateTime": {"$mod": [24 * 3600, 0]}}
+    #                     ]
+    #             },
+    #             {
+    #                 "$and":
+    #                     [
+    #                         {"DateTime": {"$gt": 1477353600}},
+    #                         {"DateTime": {"$mod": [24 * 3600, 58200]}}
+    #                     ]
+    #             }
+    #         ]
+    #     }
+    # )
+    # print(len(pred))
     with open("data/predictFullTruth.json", "r") as file:
         truth = json.load(file)
 
@@ -199,7 +201,7 @@ def test_meanNstd_yy():
     #              'VPdef(mbar)': '1.91', 'sh(g/kg)': '3.52', 'H2OC(mmol/mol)': '5.65', 'rho(g/m**3)': '1232.89',
     #              'wv(m/s)': '1.93', 'max.wv(m/s)': '4.50', 'wd(deg)': '233.30'}]
     # csv_path = mongodb.queryAll({})
-    with open("../full.json", "r") as file:
+    with open("data/full.json", "r") as file:
         csv_path = json.load(file)
     save_path = 'images/meanstd'
     if not os.path.isdir(save_path):
@@ -212,7 +214,7 @@ def test_meanNstd_yy():
 
 
 def test_wind():
-    with open("../full.json", "r") as file:
+    with open("data/full.json", "r") as file:
         csv_path = json.load(file)
     # csv_path = mongodb.queryAll({})
     # csv_path = [{'DateTime': 1232298600.0, 'p(mbar)': '978.50', 'T(degC)': '2.73', 'Tpot(K)': '277.61',
